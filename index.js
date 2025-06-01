d8e081d086eb0502e4e47449e153a3747bb6c683
HI-TECHSTUDIO2024 <vladimir.yarovoy.jr@gmail.com>
2025-06-01 09:51:05 +0200

    aa

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-// Отримуємо всі елементи <a> всередині <ul>
-const links = document.querySelectorAll('ul a');
-
-// Перебираємо кожен елемент посилання
-links.forEach(link => {
-  const href = link.getAttribute('href');
-
-  // Перевіряємо, чи посилання є зовнішнім (починається з http://, https:// або ftp://)
-  if (href && (href.startsWith('http://') || href.startsWith('https://') || href.startsWith('ftp://'))) {
-    // Якщо посилання зовнішнє, встановлюємо його колір на помаранчевий
-    link.style.color = 'orange';
-  }
-});
+const h1 = document.querySelector('h1')
+h1.
